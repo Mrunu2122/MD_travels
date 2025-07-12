8bda77fce8a949f44edd4dced288d2f9c648f40e
Mrunu2122 <mrunalsoshte212208@gmail.com>
2025-07-13 00:13:12 +0530

    render issue add

--- md_backend/settings.py
+++ md_backend/settings.py
@@ -114,6 +114,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 STATIC_URL = 'static/'
 
