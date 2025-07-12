8e61cf1dbf9744478563723113e95fa91bdfe18d
Mrunu2122 <mrunalsoshte212208@gmail.com>
2025-07-13 00:25:06 +0530

    render fix

--- md_backend/settings.py
+++ md_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2#jwjz7$!-dycnmdq9^ezs%37#m8ia4yu7=c7nronmfbsl8f6%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['md-travels.onrender.com']
 
 
 # Application definition
